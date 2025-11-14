801527778363445b1c079414842422cef47ca3ea
callistakushermyla731 <callistakushermyla731@gmail.com>
2025-11-14 13:10:56 +0700

    No. 7 Percobaan 1

--- Bioskop05.java
+++ Bioskop05.java
@@ -1,6 +1,6 @@
 public class Bioskop05 {
     public static void main(String[] args) {
-        
+
         String[][] penonton = new String[4][2];
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
@@ -19,6 +19,11 @@ public class Bioskop05 {
         System.out.println(penonton.length);
         for(String[] barisPenonton : penonton){
             System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        System.out.println("Penonton pada baris ke-3: ");
+        for(int i = 0; i < penonton[2].length; i++){
+            System.out.println(i);
         }
 
     }
