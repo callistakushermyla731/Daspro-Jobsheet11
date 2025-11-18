a0b6b58292b8028bdd647e81022e76527cbbc0cd
callistakushermyla731 <callistakushermyla731@gmail.com>
2025-11-18 18:34:56 +0700

    No. 2 Percobaan 2

--- BioskopWithScanner05.java
+++ BioskopWithScanner05.java
@@ -7,23 +7,56 @@ public class BioskopWithScanner05 {
         String nama, next;
 
         String[][] penonton = new String[4][2];
+
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama=sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris=sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom=sc.nextInt();
+            System.out.println("\nMenu");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            int pilih = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            if (pilih == 1) {
+                while (true) {
+                System.out.print("Masukkan nama: ");
+                nama=sc.nextLine();
+                System.out.print("Masukkan baris: ");
+                baris=sc.nextInt();
+                System.out.print("Masukkan kolom: ");
+                kolom=sc.nextInt();
+                sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next=sc.nextLine();
+                penonton[baris-1][kolom-1] = nama;
 
-            if (next.equalsIgnoreCase("n")) {
+                System.out.print("Input penonton lainnya? (y/n): ");
+                next=sc.nextLine();
+
+                    if (next.equalsIgnoreCase("n")) {
+                    break;
+                    }
+                }
+            }
+
+            if (pilih == 2) {
+                System.out.println("\nDaftar penonton");
+                for(int i = 0; i < penonton.length; i++){
+                    for(int j = 0; j < penonton[i].length; j++){
+                        String isi = penonton[i][j];
+                        if (isi == null) {
+                            isi = "-";
+                        }
+                        System.out.print(isi + " ");
+                    }
+                    System.out.println();
+                }
+            }
+
+            if (pilih == 3) {
                 break;
             }
         }
+
+        
     }
 }
