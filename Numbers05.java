4563fb6c759c157baf1e5c6bb2d10cdcaa2aa632
callistakushermyla731 <callistakushermyla731@gmail.com>
2025-11-18 21:33:19 +0700

    No. 4 Percobaan 3

--- Numbers05.java
+++ Numbers05.java
@@ -18,5 +18,9 @@ public class Numbers05 {
         for(int i = 0; i < myNumbers.length; i++){
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+
+        for(int i = 0; i < myNumbers.length; i++){
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     } 
 }
